5f155d1f16e1b08e70e424244c31b119313a8e68
KrankerApfel <t.rakoto76@gmail.com>
2025-07-31 15:36:56 +0200

    [Sequencer] add action play in time

--- Assets/Scripts/Sequencer.cs
+++ Assets/Scripts/Sequencer.cs
@@ -1,31 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Sequencer : MonoBehaviour
 {
+    [SerializeField]
+    private ActionSequencer actionSequencer;
+
     private float beatInterval;
     private float barInterval;
     private AudioSource audioSource;
-    
+
     public int BPM = 60;
     public int BeatsPerBar = 4;
     public int BarCount = 4;
-   
+
     public AudioClip BeatClip;
     public AudioClip BarClip;
 
-    private ActionSequencer actionSequencer;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         beatInterval = 60f / BPM;
         barInterval = beatInterval * BeatsPerBar;
         audioSource = GetComponent<AudioSource>();
-    }
-
-    public void Init(ActionSequencer s)
-    {
-        actionSequencer = s;
     }
 
     void FixedUpdate()
@@ -38,14 +36,21 @@ public class Sequencer : MonoBehaviour
             // audioSource.pitch = -1f;
             // audioSource.timeSamples = audioSource.clip.samples - 1;
             // audioSource.PlayCurrentAction();
-            
+
             audioSource.PlayOneShot(BeatClip);
         }
-        
+
         if (Time.time % barInterval < Time.fixedDeltaTime && Time.time > 0)
         {
             audioSource.PlayOneShot(BarClip);
-            Ring.Instance.MoveToNextSlot();
+            PlayAction();
         }
+    }
+
+    private void PlayAction()
+    {
+        Ring.Instance.MoveToNextSlot();
+        actionSequencer.PlayCurrentAction();
+        actionSequencer.NextAction();
     }
 }
