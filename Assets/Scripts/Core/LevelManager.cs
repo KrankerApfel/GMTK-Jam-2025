77ce05ba07cb465a87baae5b0b4947c9ef188d58
KrankerApfel <t.rakoto76@gmail.com>
2025-08-02 12:07:39 +0200

    [ Menu ] Add main menu and LevelManager

---
+++ Assets/Scripts/Core/LevelManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    public void LoadLevel(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [System.Serializable]
+    public class Menu
+    {
+        public string menuName;
+        public GameObject menuRoot;
+        public bool canBeClosed;
+    }
+
+    public Menu[] menus;
+    private Stack<Menu> menuStack = new Stack<Menu>();
+
+    void Start()
+    {
+        OpenMenu("Main");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (menuStack.Count > 1)
+            {
+                CloseMenu();
+            }
+            else if (menuStack.Count == 1 && menuStack.Peek().canBeClosed)
+            {
+                CloseMenu();
+            }
+        }
+    }
+
+    public void OpenMenu(string menuName)
+    {
+        Menu targetMenu = FindMenu(menuName);
+
+        if (targetMenu != null)
+        {
+            if (menuStack.Count > 0)
+                menuStack.Peek().menuRoot.SetActive(false);
+
+            targetMenu.menuRoot.SetActive(true);
+            menuStack.Push(targetMenu);
+        }
+        else
+        {
+            Debug.LogWarning("Menu introuvable : " + menuName);
+        }
+    }
+
+    public void CloseMenu()
+    {
+        if (menuStack.Count > 0)
+        {
+            Menu currentMenu = menuStack.Pop();
+            currentMenu.menuRoot.SetActive(false);
+
+            if (menuStack.Count > 0)
+                menuStack.Peek().menuRoot.SetActive(true);
+        }
+    }
+
+    public void OnStartGame()
+    {
+        LevelManager.Instance.LoadLevel(1); 
+    }
+
+    public void OnCredits()
+    {
+        OpenMenu("Credits");
+    }
+
+    public void OnQuitGame()
+    {
+        // close the game on build version.
+        // it's normal if it do nothing in the editor
+        // don't worry ;)
+        Application.Quit();
+    }
+
+    private Menu FindMenu(string menuName)
+    {
+        foreach (var m in menus)
+        {
+            if (m.menuName == menuName)
+                return m;
+        }
+        return null;
+    }
+}
