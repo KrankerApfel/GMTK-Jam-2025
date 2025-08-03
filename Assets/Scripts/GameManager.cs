b47a7eae94d33f0d1bae5e43e1448b883052e8f8
KrankerApfel <t.rakoto76@gmail.com>
2025-08-03 16:20:34 +0200

    Add a deadzone for when we fall
    # Conflicts:
    #	Assets/Scenes/Levels/Level_0.unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,6 +21,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip winAudio;
 
+    private bool levelFinished = false;
     private void Awake()
     {
         if (Instance == null)
@@ -53,7 +54,7 @@ public class GameManager : MonoBehaviour
             player.OnPlayerDestroyed += OnPlayerDestroyed;
         }
 
-        Debug.Log("Init");
+        levelFinished = false;
 
         if (actionSequencer != null && actionPool != null && fixedSequence != null)
         {
@@ -85,13 +86,16 @@ public class GameManager : MonoBehaviour
         {
             player.OnPlayerDestroyed -= OnPlayerDestroyed;
         }
+
+        if(!levelFinished)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 
     public void OnLevelFinished()
     {
         Sequencer.Instance.Stop();
-
+        levelFinished = true;
         if (audioSource && winAudio)
         {
             audioSource.PlayOneShot(winAudio);
@@ -104,7 +108,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private System.Collections.IEnumerator WaitAndGoToNextLevel(float delay)
+    private IEnumerator WaitAndGoToNextLevel(float delay)
     {
         yield return new WaitForSeconds(delay);
 
